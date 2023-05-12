46493af1c4773c93f788115b3116ae033248f1d7
remmy <chaloremmygmail.comgit config --global user.name remmygit config --global user.email chaloremmygmail.com>
2023-05-12 16:39:58 +0300

    set up firebase ,react enviroment and redux template

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAmVboIoPUoq7JNQ2WSAfnuAoK1cPMFOwI",
+  authDomain: "mini-netflix-eb25f.firebaseapp.com",
+  projectId: "mini-netflix-eb25f",
+  storageBucket: "mini-netflix-eb25f.appspot.com",
+  messagingSenderId: "1063379427710",
+  appId: "1:1063379427710:web:7c14cbe8a73c7ad00cd32f"
+};
