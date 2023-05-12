2e9079ead1cdb005747c68f9c9e8a306df8438c3
remmy <chaloremmygmail.comgit config --global user.name remmygit config --global user.email chaloremmygmail.com>
2023-05-12 18:14:55 +0300

    only  show nav_black after scrollY> 100

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,21 +1,36 @@
 import React from "react";
 import "./Nav.css";
+import { useEffect, useState } from "react";
 
 function Nav() {
+  const [show, handleShow] = useState(false);
+  const transitionNavBar = () => {
+    if (window.scrollY > 100) {
+      handleShow(true);
+    } else {
+      handleShow(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavBar);
+    return () => window.removeEventListener("scroll", transitionNavBar);
+  }, []);
+
   return (
-    <div className="nav nav_black">
-      <div  nav_contents>
+    <div className={`nav ${show && "nav_black"}`}>
+      <div nav_contents>
         <img
-        className="nav_logo"
+          className="nav_logo"
           src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png"
           alt="logo"
         ></img>
         <img
-        className="nav_avatar"
+          className="nav_avatar"
           src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
           alt="avatar"
         ></img>
-        <h1>This is the navbar</h1>
+       
       </div>
     </div>
   );
