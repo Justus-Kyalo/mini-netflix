ce603beb43bce04f73c6c831508a102f3d136301
remmy <chaloremmygmail.comgit config --global user.name remmygit config --global user.email chaloremmygmail.com>
2023-06-15 13:31:46 +0300

    passed props to Rows

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,18 +1,33 @@
-import React from 'react'
-import "./HomeScreen.css"
-import Nav from "./Nav"
-import Banner from './Banner'
+import React from "react";
+import "./HomeScreen.css";
+import Nav from "./Nav";
+import Banner from "./Banner";
+import requests from "../Requests";
+import Row from "./Row";
 
 function HomeScreen() {
   return (
-    <div  className='homeScreen'>
-        {/* nav */}
-        <Nav/>
-        {/* banner */}
-        <Banner />
-        {/* row */}
+    <div className="homeScreen">
+      {/* nav */}
+      <Nav />
+      {/* banner */}
+      <Banner />
+      {/* row */}
+
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMoviews} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
-  )
+  );
 }
 
-export default HomeScreen
+export default HomeScreen;
