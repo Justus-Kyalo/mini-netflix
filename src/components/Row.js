9e2dec3dadbaeac99f9f963860e82045cf5d9e78
remmy <chaloremmygmail.comgit config --global user.name remmygit config --global user.email chaloremmygmail.com>
2023-06-15 14:28:59 +0300

    rendered movie posters to respective categories

--- src/components/Row.js
+++ src/components/Row.js
@@ -3,20 +3,33 @@ import axios from "../axios";
 import "./Row.css";
 
 function Row({ title, fetchUrl, isLargeRow = false }) {
-   const [movies, setMovies] = useState([]);
-   useEffect(() => {
-     async function fetchData() {
+  const [movies, setMovies] = useState([]);
+
+  const base_url = "https://image.tmdb.org/t/p/original/";
+  useEffect(() => {
+    async function fetchData() {
       const request = await axios.get(fetchUrl);
-       setMovies(request.data.results);
+      setMovies(request.data.results);
       return request;
-     };
-     fetchData();
-  },
-   [fetchUrl]);
-   console.log(movies);
+    }
+    fetchData();
+  }, [fetchUrl]);
+  //console.log(movies);
   return (
     <div className="row">
       <h1>{title}</h1>
+      <div className="row_posters">
+        {movies.map((movie) => (
+          <img
+            className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+            key={movie.id}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          ></img>
+        ))}
+      </div>
     </div>
   );
 }
