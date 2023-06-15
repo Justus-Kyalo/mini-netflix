054a00eef0e118bc255895ccf6315e755553cf51
remmy <chaloremmygmail.comgit config --global user.name remmygit config --global user.email chaloremmygmail.com>
2023-06-15 13:28:05 +0300

    removed typo

--- src/Requests.js
+++ src/Requests.js
@@ -2,12 +2,12 @@ const API_KEY = "a610eecf8ae49b0e3499ad168ce74557";
 
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&languages=en-US`,
-  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_network=213`,
-  fetchTopRated: `/movies/top_rated?api_key=${API_KEY}&languages=en-US`,
-  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genre=28`,
-  fetchComedyMoviews: `/discover/movie?api_key=${API_KEY}&with_genre=35`,
-  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genre=27`,
-  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genre=10749`,
-  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genre=99`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&languages=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
 };
 export default requests;
