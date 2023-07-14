b67c67ce26cc9f6037f4791bcb70064096c778fa
remmy <chaloremmygmail.comgit config --global user.name remmygit config --global user.email chaloremmygmail.com>
2023-07-14 15:38:15 +0300

    if no  user render sign in page otherwise HomeScreen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import './App.css';
-import './components/HomeScreen'
-import HomeScreen from './components/HomeScreen';
+import React from "react";
+import "./App.css";
+import "./screens/HomeScreen";
+import HomeScreen from "./screens/HomeScreen";
+import "./screens/LoginScreen"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginScreen from "./screens/LoginScreen";
 
 function App() {
+  const user = null;
   return (
     <div className="app">
-     <HomeScreen/>
+      <Router>
+        {!user ? (
+         <LoginScreen/> 
+        ) : (
+          <Routes>
+            <Route exact path="/" element={<HomeScreen />} />
+          </Routes>
+        )}
+      </Router>
     </div>
   );
 }
