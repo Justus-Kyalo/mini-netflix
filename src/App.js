449b316fcfda8d58205e36cf99a02aef810decd4
remmy <chaloremmygmail.comgit config --global user.name remmygit config --global user.email chaloremmygmail.com>
2023-07-16 22:40:43 +0300

    onAuthStateChanged functionality with firebase to show the current signed in user

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import "./screens/HomeScreen";
 import HomeScreen from "./screens/HomeScreen";
-import "./screens/LoginScreen"
+import "./screens/LoginScreen";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginScreen from "./screens/LoginScreen";
+import { auth } from "./firebase";
 
 function App() {
   const user = null;
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        //logged in
+        console.log(userAuth);
+      } else {
+        //logged out
+      }
+    });
+    return unsubscribe;
+  }, []);
   return (
     <div className="app">
       <Router>
         {!user ? (
-         <LoginScreen/> 
+          <LoginScreen />
         ) : (
           <Routes>
             <Route exact path="/" element={<HomeScreen />} />
