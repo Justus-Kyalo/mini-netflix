37ab548b8c7468279b3118010ae368acb38ffb1e
remmy <chaloremmygmail.comgit config --global user.name remmygit config --global user.email chaloremmygmail.com>
2023-07-19 18:53:22 +0300

    added routing to home page and profile via nav logo and avatar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./Nav.css";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Nav() {
   const [show, handleShow] = useState(false);
+  const navigate = useNavigate();
   const transitionNavBar = () => {
     if (window.scrollY > 100) {
       handleShow(true);
@@ -21,16 +23,19 @@ function Nav() {
     <div className={`nav ${show && "nav_black"}`}>
       <div nav_contents>
         <img
+        onClick={() =>{navigate("/")}}
           className="nav_logo"
           src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png"
           alt="logo"
         ></img>
         <img
+          onClick={() => {
+            navigate("/profile");
+          }}
           className="nav_avatar"
           src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
           alt="avatar"
         ></img>
-       
       </div>
     </div>
   );
